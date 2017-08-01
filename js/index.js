b871a90acb9c83414933b1372c1efeb8a40fbf0f
Fernando Lapa <fernando.lapa@concrete.com.br>
2017-08-01 15:01:45 -0300

    Final

--- js/index.js
+++ js/index.js
@@ -9,14 +9,14 @@
 	var finalTime = 0;
 	var src = "#showCards";
 	var images = [
-		 "img/img1.png",
-		 "img/img2.png",
-		 "img/img3.png",
-		 "img/img4.png",
-		 "img/img5.png",
-		 "img/img6.png",
-		 "img/img7.png",
-		 "img/img8.png"
+		"img/img1.png",
+		"img/img2.png",
+		"img/img3.png",
+		"img/img4.png",
+		"img/img5.png",
+		"img/img6.png",
+		"img/img7.png",
+		"img/img8.png"
 	];
 
 	let c1 = app.getComponente('c1');
@@ -25,120 +25,118 @@
 
 
 
-			$ctrl.start = function ResetGame() {
-			getCards();
-			shuffle();
-			$(src + " div img").show(3000);
-			$(src + " div img").hide(400);
-			$(src + " div").css("visibility", "visible");
-			$("#success").remove();
-			boxImg = "";
-			firstTurned = "";
-			pairs = 0;
-			return false;
+	$ctrl.start = function ResetGame() {
+		getCards();
+		shuffle();
+		$(src + " div img").show(3000);
+		$(src + " div img").hide(400);
+		$(src + " div").css("visibility", "visible");
+		$("#success").remove();
+		boxImg = "";
+		firstTurned = "";
+		pairs = 0;
+		return false;
+	}
+
+
+	function RandomFunction(MaxValue, MinValue) {
+		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+	}
+	function shuffle() {
+
+		var ImgAll = $(src).children();
+		var ImgThis = $(src + " div:first-child");
+		var ImgArr = new Array();
+
+		for (var i = 0; i < ImgAll.length; i++) {
+			ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
+			ImgThis = ImgThis.next();
 		}
 
-		function calcTime(a, b){
-			finalTime = b - a;
-			return finalTime;
-		}
+		ImgThis = $(src + " div:first-child");
 
-
-
-		function turnCard() {
-			var initTime = performance.now();
-			var id = $(this).attr("id");
-
-						if ($("#" + id + " img").is(":hidden")) {
-							$(src + " div").unbind("click", turnCard);
-							$("#" + id + " img").slideDown('fast');
-							if (firstTurned == "") {
-								boxImg = id;
-								firstTurned = $("#" + id + " img").attr("src");
-								setTimeout(function() {
-									$(src + " div").bind("click", turnCard)
-								}, 400);
-							} else {
-								secondTurned = $("#" + id + " img").attr("src");
-								if (firstTurned != secondTurned) {
-									setTimeout(function() {
-										$("#" + id + " img").slideUp('fast');
-										$("#" + boxImg + " img").slideUp('fast');
-										boxImg = "";
-										firstTurned = "";
-										counterError++;
-
-									}, 700);
-								} else {
-									$("#" + id + " img").parent().css("visibility", "visible");
-									$("#" + boxImg + " img").parent().css("visibility", "visible");
-									boxImg = "";
-									firstTurned = "";
-									pairs++;
-								}
-								setTimeout(function() {
-									$(src + " div").bind("click", turnCard)
-								}, 700);
-							}
-
-							if (counterTry === parseInt(counterTry,10)) {
-
-							}
-								if (pairs == pairsTotal) {
-								 var endTime = performance.now();
-								 calcTime(initTime, endTime);
-
-
-								alert("Parabéns, você ganhou o jogo! Suas estatísticas: \n"
-									+ "\nTempo: " + finalTime + "Ms");
-
-								Save();
-
-							}
-						}
-					}
-
-		function RandomFunction(MaxValue, MinValue) {
-				return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
-			}
-		function shuffle() {
-
-			var ImgAll = $(src).children();
-			var ImgThis = $(src + " div:first-child");
-			var ImgArr = new Array();
-
-			for (var i = 0; i < ImgAll.length; i++) {
-				ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
-				ImgThis = ImgThis.next();
-			}
-
-				ImgThis = $(src + " div:first-child");
-
-			for (var z = 0; z < ImgAll.length; z++) {
+		for (var z = 0; z < ImgAll.length; z++) {
 			var RandomNumber = RandomFunction(0, ImgArr.length - 1);
 
-				$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
-				ImgArr.splice(RandomNumber, 1);
-				ImgThis = ImgThis.next();
+			$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
+			ImgArr.splice(RandomNumber, 1);
+			ImgThis = ImgThis.next();
+		}
+	}
+
+	function calcTime(a, b){
+		finalTime = b - a;
+		return finalTime;
+	}
+
+
+
+	function turnCard() {
+		var initTime = performance.now();
+		var id = $(this).attr("id");
+		if ($("#" + id + " img").is(":hidden")) {
+			$(src + " div").unbind("click", turnCard);
+			$("#" + id + " img").slideDown('fast');
+			if (firstTurned == "") {
+				boxImg = id;
+				firstTurned = $("#" + id + " img").attr("src");
+				setTimeout(function() {
+					$(src + " div").bind("click", turnCard)
+				}, 400);
+			} else {
+				secondTurned = $("#" + id + " img").attr("src");
+				if (firstTurned != secondTurned) {
+					setTimeout(function() {
+						$("#" + id + " img").slideUp('fast');
+						$("#" + boxImg + " img").slideUp('fast');
+						boxImg = "";
+						firstTurned = "";
+						counterError++;
+
+					}, 700);
+				} else {
+					$("#" + id + " img").parent().css("visibility", "visible");
+					$("#" + boxImg + " img").parent().css("visibility", "visible");
+					boxImg = "";
+					firstTurned = "";
+					pairs++;
+				}
+				setTimeout(function() {
+					$(src + " div").bind("click", turnCard)
+				}, 700);
+			}
+
+			if (counterTry === parseInt(counterTry,10)) {
+
+			}
+			if (pairs == pairsTotal) {
+				var endTime = performance.now();
+				calcTime(initTime, endTime);
+
+
+				alert("Parabéns, você ganhou o jogo! Suas estatísticas: \n"
+				+ "\nTempo: " + finalTime + "Ms");
+
+				Save();
+
 			}
 		}
+	}
 
-
-
-		$(function() {
+	$(function() {
 
 		for (var y = 1; y < 3 ; y++) {
 			$.each(images, function(i, val) {
 				$(src).append("<div id=card" + y + i + "><img src=" + val + " />");
 			});
 		}
-			$(src + " div").click(turnCard);
-			shuffle();
-		});
+		$(src + " div").click(turnCard);
+		shuffle();
+	});
 
 
 
-		function Save(){
+	function Save(){
 
 		var objLS = ("Tempo em ms :" + finalTime);
 		var tempo1 = JSON.parse(localStorage.getItem('objLS'));
@@ -146,17 +144,17 @@
 		if (tempo1 == null) {
 			localStorage.setItem('objLS', JSON.stringify(objLS));
 		} else {
-				if (objLS <= tempo1) {
-						localStorage.setItem('objLS', JSON.stringify(objLS));
-						}
-				}
+			if (objLS <= tempo1) {
+				localStorage.setItem('objLS', JSON.stringify(objLS));
+			}
 		}
+	}
 
-		function getCards(){
-			var retrievedObject = JSON.parse(localStorage.getItem('objLS'));
-				$("#ultimaEstat").html("" + retrievedObject)
-				console.log('retrievedObject: ', retrievedObject)
-		}
+	function getCards(){
+		var retrievedObject = JSON.parse(localStorage.getItem('objLS'));
+		$("#ultimaEstat").html("" + retrievedObject)
+		console.log('retrievedObject: ', retrievedObject)
+	}
 
 
 
